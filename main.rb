72cb62e31bdd117bd14408a58d1e237ad2f94495
hermanzh <herman.zhang@Herman-PC.ent.rt.verigy.net>
2015-06-15 15:27:37 +0800

    Tdd first version

--- main.rb
+++ main.rb
@@ -1,9 +1,23 @@
-class Main
-  def initialize
-    @str = "Hello world!"
+
+def getPrimeArr(num)
+  primeArr = []
+    
+    while num != 0
+    end
+    
+  if num == 1
+    primeArr
+  elsif num == 2 or num == 3
+    primeArr << num
+  elsif num%2 == 0
+    primeArr << 2
+    if num/2/2 == 2
+      primeArr << num/2/2
+      num = num/2
+    end
+    primeArr << num/2
+  elsif num % 3 == 0
+    
   end
   
-  def updateStr
-    @str = "End!"
-  end
 end
--- mainTest.rb
+++ mainTest.rb
@@ -3,16 +3,29 @@ require 'main'
 
 
 class MainTest < Test::Unit::TestCase
-  def setup
-    @obj = Main.new
+  def test_getPrimeArr_1
+    assert_equal([],getPrimeArr(1))
   end
   
-  def test_updateStr
-    setup
-    str = @obj.updateStr
-    assert_equal("End!",str)
-    assert_equal("End",str)
+  def test_getPrimeArr_2
+    assert_equal([2],getPrimeArr(2))
   end
   
+  def test_getPrimeArr_3 
+    assert_equal([3],getPrimeArr(3))
+  end
+  
+  def test_getPrimeArr_4_6
+    assert_equal([2,2],getPrimeArr(4))
+    assert_equal([2,3],getPrimeArr(6))
+  end
+  
+  def test_getPrimeArr_8
+    assert_equal([2,2,2],getPrimeArr(8))
+  end
+  
+def test_getPrimeArr_9
+  assert_equal([3,3],getPrimeArr(9))
+end
   
 end
